39eded2769f7fc33c067ec3a66d51727d1ad2389
ademtaparci <ademtaparci@gmail.com>
2021-12-11 17:52:46 +0300

    Update functions.js
    test2

--- src/functions.js
+++ src/functions.js
@@ -1,43 +1,44 @@
-function addToCart(quantity, productName="Elma" ){
+function addToCart(quantity, productName = "Elma") {
     console.log("Sepete Eklendi : " + productName + " adet :" + quantity)
 }
 
 //addToCart()
 addToCart("elma", 10)
-//addToCart("Karpuz")
+    //addToCart("Karpuz")
 
-let sayHello = () =>{
+let sayHello = () => {
     console.log("Hello World!")
 }
 
+
 sayHello()
 
-let sayHello2 = function () {
-    console.log( "Hello World 2")
+let sayHello2 = function() {
+    console.log("Hello World 2")
 }
 
- sayHello2 = function () {
-    console.log( "Hello World 3")
+sayHello2 = function() {
+    console.log("Hello World 3")
 }
 
 sayHello2();
 
 
 
-let product1 = {productName:"Elma", unitPrice:10, quantity:5}
+let product1 = { productName: "Elma", unitPrice: 10, quantity: 5 }
 
-function addToCart3(product){
+function addToCart3(product) {
     console.log("------------------")
-     console.log("Ürün  : "+product.productName)
-     console.log("Adet  : "+product.unitPrice)
-     console.log("Fiyat : "+product.quantity)
-} 
+    console.log("Ürün  : " + product.productName)
+    console.log("Adet  : " + product.unitPrice)
+    console.log("Fiyat : " + product.quantity)
+}
 
 addToCart3(product1)
 
 
-let product2 = {productName:"Elma", unitPrice:10, quantity:5}
-let product3 = {productName:"Elma", unitPrice:10, quantity:5}
+let product2 = { productName: "Elma", unitPrice: 10, quantity: 5 }
+let product3 = { productName: "Elma", unitPrice: 10, quantity: 5 }
 
 product2 = product3
 product2.productName = "KARPUZ"
@@ -46,45 +47,46 @@ console.log("***************")
 
 
 
-let addToCart4 = (products) =>{ 
+let addToCart4 = (products) => {
     console.log(products)
-    
+
 }
 
 let products = [
-    {productName:"Elma", unitPrice:10, quantity:5},
-    {productName:"armut", unitPrice:10, quantity:5},
-    {productName:"ayva", unitPrice:10, quantity:5}
+    { productName: "Elma", unitPrice: 10, quantity: 5 },
+    { productName: "armut", unitPrice: 10, quantity: 5 },
+    { productName: "ayva", unitPrice: 10, quantity: 5 }
 ]
 
 addToCart4(products)
 
 console.log("************")
-function add(bisey, ...numbers ) {
+
+function add(bisey, ...numbers) {
     let total = 0;
     for (let i = 0; i < numbers.length; i++) {
-        total = total+ numbers[i]
-        
+        total = total + numbers[i]
+
     }
 
     console.log(total)
     console.log(bisey)
 }
 
- 
+
 add(20, 30)
 
 
-let numbers = [30,50,100,500]
+let numbers = [30, 50, 100, 500]
 console.log(...numbers)
 console.log("********************************")
 console.log(Math.max(...numbers))
 
 
-let [icAnadolu,marmara, Karadeniz,[icAnadoluSehirleri, marmaraSehirleri]] = [
-    {name:"İç Anadolu", population:"20M"},
-    {name:"marmara", population:"40M"},
-    {name:"Karadeniz", population:"10M"},
+let [icAnadolu, marmara, Karadeniz, [icAnadoluSehirleri, marmaraSehirleri]] = [
+    { name: "İç Anadolu", population: "20M" },
+    { name: "marmara", population: "40M" },
+    { name: "Karadeniz", population: "10M" },
     [
         ["Ankara", "konya"],
         ["İstanbul", "Bursa"],
@@ -92,15 +94,13 @@ let [icAnadolu,marmara, Karadeniz,[icAnadoluSehirleri, marmaraSehirleri]] = [
     ]
 ]
 console.log("///////////////")
-//console.log(icAnadolu)
-//console.log(marmara.population)
+    //console.log(icAnadolu)
+    //console.log(marmara.population)
 console.log(marmaraSehirleri)
 
 console.log("///////////////")
-function someFunction(x,y,z) {
+
+function someFunction(x, y, z) {
     console.log(y)
 }
-someFunction([10000,20000],3000)
-
-
-
+someFunction([10000, 20000], 3000)
